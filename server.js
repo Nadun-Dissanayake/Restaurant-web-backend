d123b2016db958e0c2c56c486574c325c7caf842
Nadun Dissanayakke <139664686+Nadun-Dissanayake@users.noreply.github.com>
2024-06-01 09:54:23 +0530

    commented

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const  bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-require('dotenv').config();
 
 
-//import routes
+// Import routes
 const restaurantRoutes = require('./routes/restaurant');
 
-//app midleware
+// App middleware
 app.use(bodyParser.json());
 app.use(cors());
 app.use(restaurantRoutes);
 
 // Environment Variables
-const PORT = process.env.PORT || 8000;
-const DB_URL = process.env.DB_URL;
+const PORT = 8000;
+const DB_URL= "mongodb+srv://test:test123@restaurantapp.tz1auai.mongodb.net/?retryWrites=true&w=majority&appName=restaurantApp" || "mongodb://mongo:27017/restaurantApp"
+
 
 // Connect to MongoDB
-mongoose.connect(DB_URL,{
-    
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
-.then(()=>{
-    console.log('DB Connected')
+.then(() => {
+    console.log('DB Connected');
 })
-.catch((err)=> console.log('DB connection Error', err));false   
+.catch((err) => console.log('DB connection Error', err));
 
 // Start the server
-app.listen(PORT, ()=>{
-    console.log(`App is running on ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
 });
